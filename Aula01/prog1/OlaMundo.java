248f07b469d67c5378b18a741d334d41c8bb0cfb
GuilhermeBzz <79452551+GuilhermeBzz@users.noreply.github.com>
2021-02-25 09:21:55 -0300

    Alterações da Aula 1

---
+++ Aula01/prog1/OlaMundo.java
@@ -0,0 +1,7 @@
+class OlaMundo{
+    public static void main(String[] args){
+        System.out.println("Ola mundo!");
+        System.out.println("Ta certo?");
+        System.out.println("Agora sim.");
+    }
+}
