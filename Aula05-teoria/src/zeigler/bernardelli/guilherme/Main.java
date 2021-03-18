031b3fea3aeb9c20f63ea4d928949c769dfacbca
GuilhermeBzz <79452551+GuilhermeBzz@users.noreply.github.com>
2021-03-18 09:13:56 -0300

    Verificador de CPF

---
+++ Aula05-teoria/src/zeigler/bernardelli/guilherme/Main.java
@@ -0,0 +1,9 @@
+package zeigler.bernardelli.guilherme;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String cpf = "152.283.958-57";
+        System.out.println("Cpf valido: "+ ValidadorCPF.validarCpf(cpf));
+    }
+}
