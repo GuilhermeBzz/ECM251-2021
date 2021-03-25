0fd0a3d96c63fc31b678a03a7e5757f6bbea2774
GuilhermeBzz <79452551+GuilhermeBzz@users.noreply.github.com>
2021-03-25 16:15:35 -0300

    Criação do Projeto e da Classe Usuario

---
+++ AtividadeT1/src/zeigler/bernardelli/guilherme/Usuario.java
@@ -0,0 +1,22 @@
+package zeigler.bernardelli.guilherme;
+
+public class Usuario {
+    private String nome;
+    private String senha;
+    private String email;
+
+    public Usuario(String nome){
+        this.nome = nome;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nome='" + nome + '\'' +
+                ", senha='" + senha + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
