8abdcc511acb66ca517251fd94ff30733a8009df
GuilhermeBzz <79452551+GuilhermeBzz@users.noreply.github.com>
2021-04-02 15:32:49 -0300

    Finalização da main
    Criação das outras operações e exibição dos dados no final

--- Atividade T1/src/zeigler/bernardelli/guilherme/Main.java
+++ Atividade T1/src/zeigler/bernardelli/guilherme/Main.java
@@ -34,10 +34,45 @@ public class Main {
 
         String op1 = Transacao.QRCode(conta1, usuario1, 250);
 
+        //Conta 2 paga Conta 1
         if (Transacao.Transacao(conta2,conta1,op1)){
             System.out.println("Transação realizada com sucesso!");
         }
         else
             System.out.println("Desculpe, não foi possível realizar a transação. Verifique as informações inseridas, seu saldo e tente novamente.");
+
+        //Conta 3 paga Conta 1
+        if (Transacao.Transacao(conta3, conta1,op1)){
+            System.out.println("Transação realizada com sucesso!");
+        }
+        else
+            System.out.println("Desculpe, não foi possível realizar a transação. Verifique as informações inseridas, seu saldo e tente novamente.");
+
+        //Conta 2 tenta pagar conta 1 novamente (Falta saldo)
+        if (Transacao.Transacao(conta2, conta1,op1)){
+            System.out.println("Transação realizada com sucesso!");
+        }
+        else
+            System.out.println("Desculpe, não foi possível realizar a transação. Verifique as informações inseridas, seu saldo e tente novamente.");
+
+        //Criacao de novo QRCode
+        String op2 = Transacao.QRCode(conta2, usuario2, 1000);
+
+        //Conta 3 paga Conta 2 com novo QRCode
+
+        if (Transacao.Transacao(conta3, conta2,op2)){
+            System.out.println("Transação realizada com sucesso!");
+        }
+        else
+            System.out.println("Desculpe, não foi possível realizar a transação. Verifique as informações inseridas, seu saldo e tente novamente.");
+
+        //Exibir estado final
+
+
+        System.out.println(conta1.toString());
+        System.out.println(conta2.toString());
+        System.out.println(conta3.toString());
+
+
     }
 }
