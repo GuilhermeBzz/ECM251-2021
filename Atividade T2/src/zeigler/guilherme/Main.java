d6c0f58a60204306da90db030b752135c9fd0293
GuilhermeBzz <79452551+GuilhermeBzz@users.noreply.github.com>
2021-06-25 18:27:45 -0300

    Adição dos nomes na main

--- Atividade T2/src/zeigler/guilherme/Main.java
+++ Atividade T2/src/zeigler/guilherme/Main.java
@@ -2,6 +2,12 @@ package zeigler.guilherme;
 
 import zeigler.guilherme.models.Sistema;
 
+//Membros do Grupo
+
+// Carolina de Carvalho Gutierrez 18.00576-4
+// Fernando Oliveira de Souza 19.00617-9
+// Guilherme Bernardelli Zeigler 19.02453-3
+
 public class Main {
 
     public static void main(String[] args) {
