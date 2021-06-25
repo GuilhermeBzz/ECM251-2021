8ff6aeacdd5c96acfad737b244e3de8fe379e748
GuilhermeBzz <79452551+GuilhermeBzz@users.noreply.github.com>
2021-06-25 16:40:06 -0300

    Criação das interfaces
    -Apresentacao
    -PostarMensagem

--- Atividade T2/src/zeigler/guilherme/interfaces/Apresentacao.java
+++ Atividade T2/src/zeigler/guilherme/interfaces/Apresentacao.java
@@ -1,4 +1,12 @@
 package zeigler.guilherme.interfaces;
 
+/**
+ * Interface Apresentacao
+ * tem apenas o metodo Apresentacao()
+ */
 public interface Apresentacao {
+    /**
+     * Apresentar informacoes sobre o objeto que chamou o metodo
+     */
+    void Apresentacao();
 }
--- Atividade T2/src/zeigler/guilherme/interfaces/PostarMensagem.java
+++ Atividade T2/src/zeigler/guilherme/interfaces/PostarMensagem.java
@@ -1,4 +1,15 @@
 package zeigler.guilherme.interfaces;
 
+import zeigler.guilherme.enums.HorarioDoSistema;
+
+/**
+ * Interface PostarMensage
+ * tem apenas o metodo PostarMensagem
+ */
 public interface PostarMensagem {
+    /**
+     * Metodo utilizado para Postar uma mensagem de acordo com quem chamou o metodo.
+     * @param horaAtual Hora atual no sistema que esta rodando
+     */
+    void PostarMensagem(HorarioDoSistema horaAtual);
 }
