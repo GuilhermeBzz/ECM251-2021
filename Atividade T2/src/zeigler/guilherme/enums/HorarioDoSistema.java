4a415166f251fa4156b255fbb9aa2a5d2695b0cc
GuilhermeBzz <79452551+GuilhermeBzz@users.noreply.github.com>
2021-06-25 16:35:38 -0300

    Criação dos enums
    -HorarioDoSistema
    -TiposMembros

--- Atividade T2/src/zeigler/guilherme/enums/HorarioDoSistema.java
+++ Atividade T2/src/zeigler/guilherme/enums/HorarioDoSistema.java
@@ -1,4 +1,8 @@
 package zeigler.guilherme.enums;
 
+/**
+ * Possiveis horarios que o sistema pode assumir
+ */
 public enum HorarioDoSistema {
+    Regular, Extra
 }
--- Atividade T2/src/zeigler/guilherme/enums/TiposMembros.java
+++ Atividade T2/src/zeigler/guilherme/enums/TiposMembros.java
@@ -1,4 +1,9 @@
 package zeigler.guilherme.enums;
 
+
+/**
+ * Possiveis tipos (Cargos) que um membro pode ter
+ */
 public enum TiposMembros {
+    MobileMember, HeavyLifter, ScriptGuy, BigBrother
 }
